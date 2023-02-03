664fd8dd6c9ca9e6179139f57f0c898a60cc73e5
Juan Ignacio Lopez <juanignacio.a.lopez@gmail.com>
2023-02-03 16:42:59 -0300

    Pilas o stacks

---
+++ Stacks/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Stack
+Es una  estructura de tipo LIFO 
+(last in first out)
+*/
+using System.Collections;
+
+
+Stack pila = new Stack();
+pila.Push(1);
+pila.Push(5);
+pila.Push(10);
+pila.Push(3);
+
+
+int contador = pila.Count;
+Console.WriteLine(contador);
+
+var numero = pila.Pop();
+
+// recorrera los elementos del ultimo al primero
+foreach(var item in pila){
+    Console.WriteLine(item);
+}
+
+contador = pila.Count;
+Console.WriteLine(contador);
+
+if (pila.Contains(10)) {
+    Console.WriteLine("Contiene un 10");
+}
+
+// Limpia la pila
+pila.Clear();
+
