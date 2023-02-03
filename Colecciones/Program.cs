63d1bd1cb30001d6657eb784c9f420ece8a0fc60
Juan Ignacio Lopez <juanignacio.a.lopez@gmail.com>
2023-02-03 15:51:50 -0300

    init repo con colecciones

---
+++ Colecciones/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+namespace colecciones {
+    internal class Program {
+        static void Main(string[] args){
+            // ArrayList
+            ArrayList lista = new ArrayList();
+            
+            lista.Add(1);
+            lista.Add("juan");
+
+            foreach (var item in lista) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
