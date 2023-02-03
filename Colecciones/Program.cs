1fce0d2d01e8403c902890a9a666efbc9d94d62f
Juan Ignacio Lopez <juanignacio.a.lopez@gmail.com>
2023-02-03 16:15:31 -0300

    Lectura Array List

--- Colecciones/Program.cs
+++ Colecciones/Program.cs
@@ -1,18 +1,36 @@
-﻿using System;
+﻿            // Se recomienda Utilizar List en ves de array list
+using System;
 using System.Collections;
 
 namespace colecciones {
     internal class Program {
         static void Main(string[] args){
             // ArrayList
+            // Guarda objetos
             ArrayList lista = new ArrayList();
             
             lista.Add(1);
             lista.Add("juan");
 
-            foreach (var item in lista) {
-                Console.WriteLine(item);
+            // Se puede acceder los elementos por su indice.
+            var elemento = lista[0];
+
+            // cantidad de elementos de la lista
+            int contador = lista.Count;
+
+            // Instertar un elemento en una posicion especifica en una lista
+            // tener en cuenta que las listas deben ser continuas.
+            lista.Insert(contador,"Pedro");
+
+            // Remover elemento de una lista segun su indice.
+            lista.RemoveAt(0);
+
+            foreach(var elem in lista) {
+                Console.WriteLine(elem);
             }
+
+
+             
         }
     }
 }
